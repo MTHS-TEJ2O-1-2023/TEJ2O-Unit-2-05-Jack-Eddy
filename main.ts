3bfbf1f0e7eee53d417112e2eae4c0f062c3396d
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-09-26 12:17:31 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack
+ * Created on: Sep 2023
+ * This program prints the temperature on a microbit
 */
 
-basic.showString('Hello, World!')
+//variable for microbits temperature
+let microbitTemperature: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+input.onButtonPressed(Button.A, function () {
+    microbitTemperature = input.temperature()
+    microbitTemperature = microbitTemperature + 273.15
+    basic.showString(' The temperature is: ')
+    basic.showNumber(microbitTemperature)
+    basic.showString('K ')
+})
